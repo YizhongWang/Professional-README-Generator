0e7ed0cedc72aef94073a41d6efd13e7b4f2d0e4
Yizhong Wang <wang.6689@osu.edu>
2023-01-04 19:58:34 -0500

    initial commit

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -158,7 +158,7 @@ const questions = [
 // function to write README file
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/generated-README.md', fileContent, err => {
+        fs.writeFile('./README.md', fileContent, err => {
             if (err) {
                 reject(err);
                 return;
@@ -176,9 +176,7 @@ const writeFile = fileContent => {
 const init = () => {
 
     return inquirer.prompt(questions)
-    .then(readmeData => {
-        return readmeData;
-    })
+   
 }
 
 // Function call to initialize app
